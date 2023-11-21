3eeeb0ff27b55e8f72589428bb7b3f6fe3450b9f
JungManKyung <mk.jung01@gretail.com>
2023-11-21 14:35:32 +0900

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -2,9 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
 import "antd/dist/antd.css";
-
 import { HashRouter as Router } from "react-router-dom";
-
 ReactDOM.createRoot(document.getElementById("root")).render(
   <Router>
     <App />
